7b093f74c3d8ffcbcd0ee1b9ce3c35d3d2ab6576
RomuloBryner <romulobry@gmail.com>
2021-03-21 20:16:18 -0400

    mi primer commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Contador
+{
+	public static void Main()
+	{
+		int Contador = 0;
+
+		Console.Write("Hasta que numero le gustaria contar? ");
+		Contador = Int32.Parse(Console.ReadLine());
+
+
+		int Bucle(int a)
+		{
+			for (int i = 1; i <= Contador; i++)
+			{
+				Console.WriteLine(i);
+			}
+			return a;
+		}
+
+
+		Func<int, int> b = (a) =>
+		{
+			Bucle(Contador);
+			return a;
+		};
+
+		int Total = b(Contador);
+		Console.ReadKey();
+
+	}
+}
